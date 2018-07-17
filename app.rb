c2832b45742353bfe2b323c878cae95e8a9c6daf
niugrace <grace.niu.01@gmail.com>
2018-07-17 19:24:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do 
+    "My name is Grace"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Novi"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is High Hopes"
+  end 
+  
 end
+
